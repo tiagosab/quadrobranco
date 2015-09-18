e73e5cfbaedbcfe861c25f3a06da13c1a2678e5c
Tiago Saboga <tiagosaboga@gmail.com>
2015-09-18 15:02:24 +0200

    Create django.mk

---
+++ django.mk
@@ -0,0 +1,4 @@
+Getting to know Django
+----------------------
+
+So I think I have found something to study in this computer world. I came to the conclusion that Django would be a very useful addition to the set of tools that I know, and I am getting to know it.
